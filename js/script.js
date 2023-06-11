bc39034f827dfd90176d6027254a724010446333
Lautaro Aguirre <aguirre.lautaro@hotmail.com>
2023-06-11 18:52:10 -0300

    casi listo

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+function cargarPlaylistDefault() {
+  mostrarPlaylist('energy');
+}
+
+
+function mostrarPlaylist(tipo) {
+    var playlistContainer = document.getElementById("playlistContainer");
+  
+    // Eliminar cualquier iframe de playlist existente
+    playlistContainer.innerHTML = "";
+  
+    // Crear y agregar el iframe correspondiente según el tipo seleccionado
+    if (tipo === "chill") {
+      playlistContainer.innerHTML = '<iframe src="https://open.spotify.com/embed/artist/69pKWucH8a724g60GjxW9R?utm_source=generator" width="100%" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
+    } else if (tipo === "energy") {
+      playlistContainer.innerHTML = '<iframe src="https://open.spotify.com/embed/playlist/7ke8MsoH2gF3zNguHGKU38?utm_source=generator" width="100%" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
+    } else if (tipo === "strong") {
+      playlistContainer.innerHTML = '<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1E4jY1WOSNjgxX?utm_source=generator" width="100%" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
+    }
+  }
